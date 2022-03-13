9fb6ea85212e2121506780f92ae45c37e424be28
Luke Browning <browningluke01@gmail.com>
2022-03-12 21:12:37 -0700

    feat(config): Add load & validate Config methods + yaml package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,12 @@
 package config
 
 import (
+	"errors"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 	"mangathrV2/internal/sources/connections/mangadex"
+	"strings"
 )
 
 type Config struct {
@@ -31,4 +36,58 @@ type Config struct {
 		Mangadex mangadex.Config
 	}
 
+}
+
+func (c* Config) Load(path string) error {
+
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	if err = c.validate(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c* Config) validate() error {
+	if !validateDatabaseDriver(c.Database.Driver) {
+		return errors.New("InvalidDatabaseError: " + c.Database.Driver + " is not a valid database.")
+	}
+	if !validateMetadataAgent(c.Metadata.Agent) {
+		return errors.New("InvalidMetadataAgentError: " + c.Metadata.Agent + " is not a valid agent.")
+	}
+	if !validateMetadataLocation(c.Metadata.Location) {
+		return errors.New("InvalidMetadataLocationError: " + c.Metadata.Location + " is not a valid location.")
+	}
+	return nil
+}
+
+
+func validateDatabaseDriver(driver string) bool {
+	return isInSlice(driver, []string{"sqlite"})
+}
+
+func validateMetadataAgent(agent string) bool {
+	return isInSlice(agent, []string{"comicinfo", "json"})
+}
+
+func validateMetadataLocation(location string) bool {
+	return isInSlice(location, []string{"internal", "external", "both"})
+}
+
+
+func isInSlice(s string, slice []string) bool {
+	for _, v := range slice {
+		if v == strings.ToLower(s) {
+			return true
+		}
+	}
+	return false
 }
