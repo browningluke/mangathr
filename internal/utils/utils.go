2e5cf6b956b2458354a8727b5834ce6521fc503f
Luke Browning <browningluke01@gmail.com>
2022-03-24 19:25:09 -0700

    Add FindInSlice util function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -10,3 +10,18 @@ func RaiseError(err error) {
 	ui.PrintlnColor(fmt.Sprint(err), ui.Red)
 	syscall.Exit(1)
 }
+
+func FindInSlice(list interface{}, match interface{}) (interface{}, bool) {
+	switch list.(type) {
+	case []string:
+		for _, item := range list.([]string) {
+			if item == match.(string) {
+				return item, true
+			}
+		}
+		return nil, false
+	default:
+		fmt.Println("unknown")
+	}
+	return nil, false
+}
