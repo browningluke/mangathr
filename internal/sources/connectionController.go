cb61818a945b9ff346f1ca10196d521ad4cdef86
Luke Browning <browningluke01@gmail.com>
2022-03-25 22:29:25 -0700

    source(Mangadex): Add connection example

--- internal/sources/connectionController.go
+++ internal/sources/connectionController.go
@@ -1,1 +1,21 @@
 package sources
+
+import (
+	"mangathrV2/internal/sources/mangadex"
+)
+
+type Connection interface {
+	GetConnectionName() string
+}
+
+func NewConnection(name string) Connection {
+	m := map[string]func() Connection{
+		"mangadex": func() Connection { return mangadex.NewConnection() },
+	}
+
+	connection, ok := m[name]
+	if !ok {
+		panic("Passed connection name not in map")
+	}
+	return connection()
+}
--- internal/sources/mangadex/connection.go
+++ internal/sources/mangadex/connection.go
@@ -1,1 +1,15 @@
 package mangadex
+
+import "fmt"
+
+type Connection struct {
+}
+
+func NewConnection() *Connection {
+	fmt.Println("Created a mangadex connection")
+	return &Connection{}
+}
+
+func (c *Connection) GetConnectionName() string {
+	return "Mangadex"
+}
