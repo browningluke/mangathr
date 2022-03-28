20d6aef7324be2c3393d2c3aab603ce9b5b4db93
Luke Browning <browningluke01@gmail.com>
2022-03-27 17:37:36 -0700

    refactor(ui): Move checkboxes to utils module

--- internal/commands/download/download.go
+++ internal/commands/download/download.go
@@ -2,49 +2,14 @@ package download
 
 import (
 	"fmt"
-	"github.com/AlecAivazis/survey/v2"
 	"mangathrV2/internal/config"
 	"mangathrV2/internal/downloader"
 	"mangathrV2/internal/sources"
+	"mangathrV2/internal/utils/ui"
 )
 
-func SingleCheckboxes(label string, opts []string) string {
-	var res string
-	prompt := &survey.Select{
-		Message: label,
-		Options: opts,
-	}
-	err := survey.AskOne(prompt, &res, survey.WithIcons(func(icons *survey.IconSet) {
-		icons.Question.Text = ""
-		icons.Question.Format = "yellow+hb"
-	}))
-	if err != nil {
-		panic(err)
-	}
-
-	return res
-}
-
-func Checkboxes(label string, opts []string) []string {
-	res := []string{}
-	prompt := &survey.MultiSelect{
-		Message:  label,
-		Options:  opts,
-		PageSize: 10,
-	}
-	err := survey.AskOne(prompt, &res, survey.WithIcons(func(icons *survey.IconSet) {
-		icons.Question.Text = ""
-		icons.Question.Format = "yellow+hb"
-	}))
-	if err != nil {
-		panic(err)
-	}
-
-	return res
-}
-
 func SelectManga(titles []string) string {
-	selection := SingleCheckboxes(
+	selection := ui.SingleCheckboxes(
 		"Select Manga:",
 		titles,
 	)
@@ -53,26 +18,8 @@ func SelectManga(titles []string) string {
 }
 
 func SelectChapters(titles []string, mangaTitle string, sourceName string) []string {
-	survey.MultiSelectQuestionTemplate = `
-{{- define "option"}}
-    {{- if eq .SelectedIndex .CurrentIndex }}{{color .Config.Icons.SelectFocus.Format }}{{ .Config.Icons.SelectFocus.Text }}{{color "reset"}}{{else}} {{end}}
-    {{- if index .Checked .CurrentOpt.Index }}{{color .Config.Icons.MarkedOption.Format }} {{ .Config.Icons.MarkedOption.Text }} {{else}}{{color .Config.Icons.UnmarkedOption.Format }} {{ .Config.Icons.UnmarkedOption.Text }} {{end}}
-    {{- color "reset"}}
-    {{- " "}}{{- .CurrentOpt.Value}}
-{{end}}
-{{- if .ShowHelp }}{{- color .Config.Icons.Help.Format }}{{ .Config.Icons.Help.Text }} {{ .Help }}{{color "reset"}}{{"\n"}}{{end}}
-{{- color .Config.Icons.Question.Format }}{{ .Config.Icons.Question.Text }} {{color "reset"}}
-{{- color "default+hb"}}{{ .Message }}{{ .FilterMessage }}{{color "reset"}}
-{{- if .ShowAnswer}}{{color "cyan"}} {{"✔"}}{{color "reset"}}{{"\n"}}
-{{- else }}
-	{{- "  "}}{{- color "cyan"}}[Use arrows to move, space to select, <right> to all, <left> to none, type to filter{{- if and .Help (not .ShowHelp)}}, {{ .Config.HelpInput }} for more help{{end}}]{{color "reset"}}
-  {{- "\n"}}
-  {{- range $ix, $option := .PageEntries}}
-    {{- template "option" $.IterateOption $ix $option}}
-  {{- end}}
-{{- end}}`
 
-	selections := Checkboxes(
+	selections := ui.Checkboxes(
 		fmt.Sprintf("\rTitle: %s\nSource: %s\n# of chapters: %d\nSelect chapters",
 			mangaTitle, sourceName, len(titles)),
 		titles,
--- internal/utils/ui/ui.go
+++ internal/utils/ui/ui.go
@@ -1,8 +1,12 @@
 package ui
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+)
 
 type Color int
+
 const (
 	Red Color = iota
 	Green
@@ -24,4 +28,58 @@ func PrintlnColor(s string, c Color) {
 
 func PrintColor(s string, c Color) {
 	fmt.Printf("%s%s%s", c, s, "\033[0m")
+}
+
+func Checkboxes(label string, opts []string) []string {
+	survey.MultiSelectQuestionTemplate = `
+{{- define "option"}}
+    {{- if eq .SelectedIndex .CurrentIndex }}{{color .Config.Icons.SelectFocus.Format }}{{ .Config.Icons.SelectFocus.Text }}{{color "reset"}}{{else}} {{end}}
+    {{- if index .Checked .CurrentOpt.Index }}{{color .Config.Icons.MarkedOption.Format }} {{ .Config.Icons.MarkedOption.Text }} {{else}}{{color .Config.Icons.UnmarkedOption.Format }} {{ .Config.Icons.UnmarkedOption.Text }} {{end}}
+    {{- color "reset"}}
+    {{- " "}}{{- .CurrentOpt.Value}}
+{{end}}
+{{- if .ShowHelp }}{{- color .Config.Icons.Help.Format }}{{ .Config.Icons.Help.Text }} {{ .Help }}{{color "reset"}}{{"\n"}}{{end}}
+{{- color .Config.Icons.Question.Format }}{{ .Config.Icons.Question.Text }} {{color "reset"}}
+{{- color "default+hb"}}{{ .Message }}{{ .FilterMessage }}{{color "reset"}}
+{{- if .ShowAnswer}}{{color "cyan"}} {{"✔"}}{{color "reset"}}{{"\n"}}
+{{- else }}
+	{{- "  "}}{{- color "cyan"}}[Use arrows to move, space to select, <right> to all, <left> to none, type to filter{{- if and .Help (not .ShowHelp)}}, {{ .Config.HelpInput }} for more help{{end}}]{{color "reset"}}
+  {{- "\n"}}
+  {{- range $ix, $option := .PageEntries}}
+    {{- template "option" $.IterateOption $ix $option}}
+  {{- end}}
+{{- end}}`
+
+	var res []string
+	prompt := &survey.MultiSelect{
+		Message:  label,
+		Options:  opts,
+		PageSize: 10,
+	}
+	err := survey.AskOne(prompt, &res, survey.WithIcons(func(icons *survey.IconSet) {
+		icons.Question.Text = ""
+		icons.Question.Format = "yellow+hb"
+	}))
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
+
+func SingleCheckboxes(label string, opts []string) string {
+	var res string
+	prompt := &survey.Select{
+		Message: label,
+		Options: opts,
+	}
+	err := survey.AskOne(prompt, &res, survey.WithIcons(func(icons *survey.IconSet) {
+		icons.Question.Text = ""
+		icons.Question.Format = "yellow+hb"
+	}))
+	if err != nil {
+		panic(err)
+	}
+
+	return res
 }
