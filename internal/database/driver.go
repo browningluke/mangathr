4b7cc2232dd4b85400c0a4d170336f9f18e40acf
Luke Browning <browningluke01@gmail.com>
2022-04-12 19:22:43 -0700

    feat(db): Add basic database module

---
+++ internal/database/driver.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"mangathrV2/ent"
+)
+
+const (
+	SQLITE = iota
+	// PSQL
+)
+
+type Driver struct {
+	client *ent.Client
+	ctx    context.Context
+}
+
+func GetDriver(driver int) (*Driver, error) {
+	d := Driver{}
+
+	// Extract driver information
+	driverName := ""
+	options := ""
+
+	switch driver {
+	case SQLITE:
+		driverName = "sqlite3"
+		options = "file:examples/db.sqlite?cache=shared&_fk=1"
+		break
+	default:
+		return nil, fmt.Errorf("%s", "Request driver not implemented")
+	}
+
+	// Open driver
+	client, err := ent.Open(driverName, options)
+	if err != nil {
+		return nil, err
+	}
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		return nil, err
+	}
+
+	d.client = client
+	d.ctx = context.Background()
+	return &d, nil
+}
+
+func (d *Driver) Close() error {
+	return d.client.Close()
+}
