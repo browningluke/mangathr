2f063981f154fdc8d66ac33561a9aeddebabf81f
Luke Browning <browningluke01@gmail.com>
2022-06-14 16:19:14 -0700

    feat(db): Add db driver update function

--- internal/database/update.go
+++ internal/database/update.go
@@ -1,1 +1,16 @@
 package database
+
+import (
+	"fmt"
+	"mangathrV2/ent"
+	"mangathrV2/internal/logging"
+)
+
+func (d *Driver) UpdateManga(mangaUpdate *ent.MangaUpdateOne) (*ent.Manga, error) {
+	manga, err := mangaUpdate.Save(d.ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating manga: %w", err)
+	}
+	logging.Debugln("manga was updated: ", manga)
+	return manga, nil
+}
