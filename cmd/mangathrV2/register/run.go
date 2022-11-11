b962c4cb4baa1977fe7077c387b37fa62d7cc449
Luke Browning <browningluke01@gmail.com>
2022-11-11 01:20:50 -0800

    use new menu structure for register command

--- cmd/mangathrV2/register/run.go
+++ cmd/mangathrV2/register/run.go
@@ -21,6 +21,7 @@ func closeDatabase() {
 		logging.Errorln(err)
 		ui.Error("Unable to close database.")
 	}
+	fmt.Println("Closing database")
 }
 
 type options struct {
@@ -48,92 +49,7 @@ func generateString(opts *options, prompt string) string {
 		chapterTitles[len(chapterTitles)-1], opts.mapping, strings.Join(opts.filteredGroups, ", "), prompt)
 }
 
-func handleRegisterMenu(opts *options, driver *database.Driver) bool {
-	confirm, err := ui.ConfirmPrompt("Register this manga?")
-	if err != nil {
-		logging.ExitIfErrorWithFunc(&logging.ScraperError{
-			Error: err, Message: "An error occurred while getting input", Code: 0,
-		}, closeDatabase)
-	}
-
-	if confirm {
-		// Start the registration process
-		fmt.Println("confirmed")
-
-		mangaID := (*opts.scraper).MangaID()
-		source := (*opts.scraper).ScraperName()
-
-		manga, err := driver.CreateManga(mangaID, opts.title, source, opts.mapping, opts.filteredGroups)
-		if err != nil {
-			logging.ExitIfErrorWithFunc(&logging.ScraperError{
-				Error: err, Message: "An error occurred when adding Manga to database", Code: 0,
-			}, closeDatabase)
-		}
-
-		chapters, scraperErr := (*opts.scraper).Chapters()
-		logging.ExitIfErrorWithFunc(scraperErr, closeDatabase)
-
-		for _, c := range chapters {
-			err := driver.CreateChapter(c.ID, c.Metadata.Num, c.Metadata.Title, manga)
-			if err != nil {
-				panic(err)
-			}
-		}
-
-	} else {
-		// return
-		fmt.Println("cancelled")
-	}
-
-	return false
-}
-
-func handleCustomizeMenu(opts *options) bool {
-	option, uierr := ui.SingleCheckboxes("Select an option",
-		[]string{"Change mapping", "Filter groups", "Back"})
-	if uierr != nil {
-		logging.ExitIfErrorWithFunc(&logging.ScraperError{
-			Error: uierr, Message: "An error occurred while getting input", Code: 0,
-		}, closeDatabase)
-	}
-
-	switch option {
-	case "Change mapping":
-		res, err := ui.InputPrompt("Map to:")
-		if err != nil {
-			logging.ExitIfErrorWithFunc(&logging.ScraperError{
-				Error: err, Message: "An error occurred while getting input", Code: 0,
-			}, closeDatabase)
-		}
-
-		opts.mapping = downloader.CleanPath(res)
-		return true
-	case "Filter groups":
-		groups, err := (*opts.scraper).GroupNames()
-		logging.ExitIfErrorWithFunc(err, closeDatabase)
-
-		selectedGroups, uierr := ui.Checkboxes("Select groups to filter: ", groups)
-		if uierr != nil {
-			logging.ExitIfErrorWithFunc(&logging.ScraperError{
-				Error: uierr, Message: "An error occurred while getting input", Code: 0,
-			}, closeDatabase)
-		}
-
-		opts.filteredGroups = selectedGroups
-
-		err = (*opts.scraper).FilterGroups(selectedGroups)
-		logging.ExitIfErrorWithFunc(err, closeDatabase)
-		return true
-	case "Back":
-		return true
-	default:
-		panic("Option selected not in list")
-	}
-
-	return false
-}
-
-func promptMainMenu(args *registerOpts, config *config.Config, driver *database.Driver) {
+func findManga(args *registerOpts, config *config.Config) (options, bool) {
 	// Do manga scraping
 	scraper := sources.NewScraper(args.Source, config)
 	titles, err := scraper.Search(args.Query)
@@ -165,26 +81,116 @@ func promptMainMenu(args *registerOpts, config *config.Config, driver *database.
 	if exists, _ := driver.CheckMangaExistence(scraper.MangaID()); exists {
 		fmt.Printf("Title: %s\nSource: %s\n", mangaTitle, scraper.ScraperName())
 		ui.PrintlnColor(ui.Yellow, "Manga is already registered. Exiting...")
+		return options{}, true
+	}
+
+	return opts, false
+}
+
+func handleMenu(args *registerOpts, config *config.Config, driver *database.Driver) {
+	opts, exists := findManga(args, config)
+	if exists {
 		return
 	}
 
-	for {
-		option, err := ui.SingleCheckboxes(generateString(&opts, "Select an option"),
-			[]string{"Register", "Customize"})
-		if err != nil {
-			logging.ExitIfErrorWithFunc(&logging.ScraperError{
-				Error: err, Message: "An error occurred while getting input", Code: 0,
-			}, closeDatabase)
-		}
+	// Define Customize panel
+	customizePanel := ui.NewPanel().
+		SetPrompt(func() string {
+			return generateString(&opts, "Select an option")
+		})
 
-		if option == "Register" {
-			if loop := handleRegisterMenu(&opts, driver); !loop {
-				break
-			}
-		} else if option == "Customize" {
-			if loop := handleCustomizeMenu(&opts); !loop {
-				break
-			}
-		}
-	}
+	customizePanel.
+		AddOption("Change mapping").
+		InputHandler("Map to:",
+			func(i string) {
+				// Handle input
+				opts.mapping = downloader.CleanPath(i)
+			},
+			func(err error) {
+				// Handle error
+				logging.ExitIfErrorWithFunc(&logging.ScraperError{
+					Error: err, Message: "An error occurred while getting input", Code: 0,
+				}, closeDatabase)
+			},
+		)
+
+	customizePanel.
+		AddOption("Filter groups").
+		CheckboxHandler("Select groups to filter: ",
+			func() []string {
+				// Generate options to display in checkboxes
+				groups, err := (*opts.scraper).GroupNames()
+				logging.ExitIfErrorWithFunc(err, closeDatabase)
+
+				return groups
+			},
+			func(i []string) {
+				// Handle selected options
+				opts.filteredGroups = i
+				err := (*opts.scraper).FilterGroups(i)
+				logging.ExitIfErrorWithFunc(err, closeDatabase)
+			},
+			func(err error) {
+				// Handle error
+				logging.ExitIfErrorWithFunc(&logging.ScraperError{
+					Error: err, Message: "An error occurred while getting input", Code: 0,
+				}, closeDatabase)
+			},
+		)
+
+	// Define Main panel
+	mainPanel := ui.NewPanel().
+		SetPrompt(func() string {
+			return generateString(&opts, "Select an option")
+		})
+
+	mainPanel.
+		AddOption("Register").
+		Terminator().
+		ConfirmationHandler("Register this manga?",
+			func() {
+				// Handle yes
+				// Start the registration process
+				fmt.Println("confirmed")
+
+				mangaID := (*opts.scraper).MangaID()
+				source := (*opts.scraper).ScraperName()
+
+				manga, err := driver.CreateManga(mangaID, opts.title, source, opts.mapping, opts.filteredGroups)
+				if err != nil {
+					logging.ExitIfErrorWithFunc(&logging.ScraperError{
+						Error: err, Message: "An error occurred when adding Manga to database", Code: 0,
+					}, closeDatabase)
+				}
+
+				chapters, scraperErr := (*opts.scraper).Chapters()
+				logging.ExitIfErrorWithFunc(scraperErr, closeDatabase)
+
+				for _, c := range chapters {
+					err := driver.CreateChapter(c.ID, c.Metadata.Num, c.Metadata.Title, manga)
+					if err != nil {
+						panic(err)
+					}
+				}
+			},
+			func() {
+				// Handle no
+				fmt.Println("cancelled")
+			},
+			func(err error) {
+				// Handle error
+				logging.ExitIfErrorWithFunc(&logging.ScraperError{
+					Error: err, Message: "An error occurred while getting input", Code: 0,
+				}, closeDatabase)
+			},
+		)
+
+	mainPanel.
+		AddOption("Customize").
+		PanelHandler(customizePanel)
+
+	// This is a blocking call
+	mainPanel.Start()
+
+	fmt.Println("Doing cleanup")
 }
